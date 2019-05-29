aff6bdf4bc89dcbbdebcc8b4f86552afe3b5166a
cranium44 <adabeechaim@gmail.com>
2019-05-30 00:52:34 +0100

    began work on the main page

--- js/login.js
+++ js/login.js
@@ -1,23 +1,25 @@
 $(function(){
     $('#login').on('click', function(){
-        var username = $('#username').value;
-        var password = $('#password').value;
+        var username = $('#username').val();
+        var password = $('#password').val();
         console.log("form input "+username+ " "+password)
 
-        //make get request
-        // $("button").click(function(){
-        //     $.ajax({
-        //         type: 'GET',
-        //         url: "http://localhost:3000/login",
-            
-        //         success: function(result){
-        //             console.log(result);
-        //             if (result.username === username) {
-        //                 console.log('success');
-        //             }
-        //         }
-        //     });
-        // });
         
+            $.ajax({
+                type: 'GET',
+                url: "http://localhost:3000/login",
+            
+                success: function(result){
+                    console.log(result);
+                    if (result.username === username) {
+                        console.log('success');
+                        window.location = "pages/main.html"
+                    }
+                },
+                error: function(){
+                    console.error("incorrect login");
+                }
+            });
+
     })
 });
