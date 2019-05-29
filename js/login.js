0111d7d0d116852a6d609b8be96a129edffe81d8
cranium44 <adabeechaim@gmail.com>
2019-05-29 17:53:19 +0100

    added new dependencies

--- js/login.js
+++ js/login.js
@@ -1,3 +1,23 @@
 $(function(){
-    $('#')
+    $('#login').on('click', function(){
+        var username = $('#username').value;
+        var password = $('#password').value;
+        console.log("form input "+username+ " "+password)
+
+        //make get request
+        // $("button").click(function(){
+        //     $.ajax({
+        //         type: 'GET',
+        //         url: "http://localhost:3000/login",
+            
+        //         success: function(result){
+        //             console.log(result);
+        //             if (result.username === username) {
+        //                 console.log('success');
+        //             }
+        //         }
+        //     });
+        // });
+        
+    })
 });
